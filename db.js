36deb2eee485f5e1482ed0001b4210ccf9daad11
Yoploo <thierry.yoran@gmail.com>
2024-02-21 16:03:02 +0100

    modify app

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  '', // Database name
-  '', // User
-  '', // Password
+  DATABASE_NAME, // Database name
+  DATABASE_USER, // User
+  DATABASE_PASSWORD, // Password
   {
-    host: '', // Host
+    host: HOST_NAME, // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
